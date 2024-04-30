7db0bf4b934c0d9a071a3bae7ba2891e396f36f9
Anirudh Sathish <81130487+Anirudh-Sathish@users.noreply.github.com>
2024-04-30 10:14:42 +0530

    Feat: Modify to get struct field information

--- data_layout_assignment/FieldStatus/FieldStatus.cpp
+++ data_layout_assignment/FieldStatus/FieldStatus.cpp
@@ -11,6 +11,7 @@
 #include<iostream>
 #include<vector>
 #include<map>
+#include <unordered_map>
 using namespace std;
 
 using namespace llvm;
@@ -21,9 +22,18 @@ namespace
 
 	struct MyPass : public PassInfoMixin<MyPass>
 	{
-		
-		PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM)
+		std::unordered_map<StructType*, unsigned> StructFieldCounts;
+		void collectStructFieldCounts(Module &M)
 		{
+			for (auto &Global : M.getGlobalList())
+			{
+				if (auto *StructType = dyn_cast<StructType>(Global.getValueType())) {
+				StructFieldCounts[StructType] = StructType->getNumElements();
+				}
+			}
+    	}
+		PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM)
+		{			
 			LLVMContext &context = M.getContext();
 			for(auto &F: M)
 			{
@@ -36,10 +46,19 @@ namespace
 							if(GEPInst->getNumIndices()>1)
 							{
 								errs()<<I<<"\n";
+								StructType *StructTy = dyn_cast<StructType>(GEPInst->getPointerOperandType()->getPointerElementType());
+								if (StructTy)
+								{
+								StructFieldCounts[StructTy]++;
+								}
 							}
 						}
 					}
 				}
+			}
+			for (auto &Pair : StructFieldCounts)
+			{
+				errs() << "Struct: " << *Pair.first << ", Field Count: " << Pair.second << "\n";
 			}
 			return PreservedAnalyses::none();
 		}
