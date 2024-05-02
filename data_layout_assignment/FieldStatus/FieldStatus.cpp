aad62359a8da98d6704ec2998dfce322d5f09690
Anirudh-Sathish <anirudhsathish@gmail.com>
2024-05-02 15:41:27 +0530

    Feat:Update code to read struct field access

--- data_layout_assignment/FieldStatus/FieldStatus.cpp
+++ data_layout_assignment/FieldStatus/FieldStatus.cpp
@@ -1,5 +1,7 @@
 /*FieldStatus Implementation*/
 #include "llvm/IR/LegacyPassManager.h"
+
+
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
@@ -8,6 +10,7 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/CFG.h"
 #include "llvm/IR/Verifier.h"
+#include "llvm/Analysis/LoopInfo.h"
 #include<iostream>
 #include<vector>
 #include<map>
@@ -23,42 +26,76 @@ namespace
 	struct MyPass : public PassInfoMixin<MyPass>
 	{
 		std::unordered_map<StructType*, unsigned> StructFieldCounts;
-		void collectStructFieldCounts(Module &M)
+		void collectStructFieldCounts(Module &M, map<string,int>* fieldCountMap)
 		{
-			for (auto &Global : M.getGlobalList())
+			
+			for(Type *type: M.getIdentifiedStructTypes())
 			{
-				if (auto *StructType = dyn_cast<StructType>(Global.getValueType())) {
-				StructFieldCounts[StructType] = StructType->getNumElements();
+				if (StructType *structType = dyn_cast<StructType>(type))
+				{
+					for (unsigned i = 0; i < structType->getNumElements(); ++i)
+					{
+						Type *fieldType = structType->getElementType(i);
+						string fieldName = structType->getName().str()+ std::to_string(i);
+						// Map the field name to 0
+						(*(fieldCountMap))[fieldName] = 0;
+                	}
+					
 				}
 			}
     	}
+		void inspectGEP(Module &M, map<string,int>* fieldCountMap,GetElementPtrInst *GEP)
+		{
+			Type *source_type = GEP->getSourceElementType();
+            Value *ptr_operand = GEP->getPointerOperand();
+			if(GetElementPtrInst *GEPInst = dyn_cast<GetElementPtrInst>(ptr_operand))
+			{
+				inspectGEP(M,fieldCountMap,GEPInst);
+			}
+			if (StructType *structType = dyn_cast<StructType>(source_type))
+			{
+				APInt ap_offset(64, 0, false);
+                GEP->accumulateConstantOffset(M.getDataLayout(), ap_offset);
+                int64_t offset_result = ap_offset.getSExtValue()/4;
+                string fieldName = structType->getName().str() +to_string(offset_result);
+                (*(fieldCountMap))[fieldName] +=1;
+			}
+			
+		}
+		void incrementFieldCount(Module &M,BasicBlock &BB, map<string,int>* fieldCountMap)
+		{
+			for(auto &I: BB)
+			{
+				if (GetElementPtrInst *GEP = dyn_cast<GetElementPtrInst>(&I))
+				{
+					errs()<<I<<"\n";
+					inspectGEP(M,fieldCountMap,GEP);
+					errs()<<"\n";
+				}
+				
+			}
+		}
+		void processFunction(Module &M, Function &F, map<string,int>* fieldCountMap)
+		{
+			for(auto &BB: F)
+			{
+				incrementFieldCount(M,BB,fieldCountMap);
+			}
+		}
+	
 		PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM)
 		{			
 			LLVMContext &context = M.getContext();
-			for(auto &F: M)
+			map<string,int>fieldCountMap; 
+			collectStructFieldCounts(M,&fieldCountMap);
+			// getFieldCount(M,&fieldCountMap);
+			for(auto &F:M)
 			{
-				for(auto &BB: F)
-				{
-					for(auto &I: BB)
-					{
-						if(GetElementPtrInst *GEPInst = dyn_cast<GetElementPtrInst>(&I))
-						{
-							if(GEPInst->getNumIndices()>1)
-							{
-								errs()<<I<<"\n";
-								StructType *StructTy = dyn_cast<StructType>(GEPInst->getPointerOperandType()->getPointerElementType());
-								if (StructTy)
-								{
-								StructFieldCounts[StructTy]++;
-								}
-							}
-						}
-					}
-				}
+				processFunction(M,F,&fieldCountMap);
 			}
-			for (auto &Pair : StructFieldCounts)
+			for(auto field: fieldCountMap)
 			{
-				errs() << "Struct: " << *Pair.first << ", Field Count: " << Pair.second << "\n";
+				errs()<<"Field: "<<field.first<<"Count: "<<field.second<<"\n";
 			}
 			return PreservedAnalyses::none();
 		}
