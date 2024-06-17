cddb9fc081a8bf0eba203061c78dd5b0fe12fbf7
Anirudh-Sathish <anirudhsathish@gmail.com>
2024-06-17 04:27:35 +0000

    Refactor: refactor the code for readability

--- mlir_pass_assignment/counter.cpp
+++ mlir_pass_assignment/counter.cpp
@@ -1,8 +1,8 @@
 /*
 Counts and prints number of operations, functions and regions
 */
-#include "mlir/Dialect/SCF/Transforms/Passes.h"
 
+#include "mlir/Dialect/SCF/Transforms/Passes.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/Dialect/SCF/Transforms/Transforms.h"
 #include "mlir/IR/BuiltinOps.h"
@@ -12,9 +12,10 @@ Counts and prints number of operations, functions and regions
 #include "mlir/Pass/PassManager.h" 
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 
-namespace mlir {
-#define GEN_PASS_DEF_SCFCOUNTER
-#include "mlir/Dialect/SCF/Transforms/Passes.h.inc"
+namespace mlir
+{
+  #define GEN_PASS_DEF_SCFCOUNTER
+  #include "mlir/Dialect/SCF/Transforms/Passes.h.inc"
 } // namespace mlir
 using namespace llvm;
 using namespace mlir;
@@ -28,24 +29,27 @@ namespace
     unsigned numRegions = 0;
     void countRegions(Operation *op)
     {
-      // Count the regions directly attached to this operation
       numRegions += op->getNumRegions();
-
-      // Recursively count regions in nested operations
-      for (Region &region : op->getRegions()) {
-        for (Block &block : region) {
-          block.walk([&](Operation *nestedOp) {
+      for (Region &region : op->getRegions())
+      {
+        for (Block &block : region)
+        {
+          block.walk([&](Operation *nestedOp)
+          {
             countRegions(nestedOp);
           });
         }
       }
     }
 
-    void runOnOperation() override {
+    void runOnOperation() override
+    {
       numOperations = 0;
       numRegions = 0;
-      getOperation()->walk([&](Operation *op) {
-      if (isa<mlir::func::FuncOp>(op)) {
+      getOperation()->walk([&](Operation *op)
+      {
+      if (isa<mlir::func::FuncOp>(op))
+      {
         numFunctions++;
       }
       numOperations++;
@@ -59,7 +63,8 @@ namespace
 };
 }
 
-std::unique_ptr<Pass> mlir::createCounterPass() {
+std::unique_ptr<Pass> mlir::createCounterPass()
+{
   return std::make_unique<OpCounter>();
 }
 
